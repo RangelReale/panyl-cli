c8189fb7f0994ba4856967c1d457940629b0bc22
Rangel Reale <rangelspam@gmail.com>
2024-12-14 07:35:48 -0300

    context

--- output.go
+++ output.go
@@ -20,18 +20,18 @@ func (o *Output) OnResult(p *panyl.Process) (cont bool) {
 	var out bytes.Buffer
 
 	// level
-	level := p.Metadata.StringValue(panyl.Metadata_Level)
+	level := p.Metadata.StringValue(panyl.MetadataLevel)
 	if level == "" {
 		level = "unknown"
 	}
 
 	// timestamp
-	if ts, ok := p.Metadata[panyl.Metadata_Timestamp]; ok {
+	if ts, ok := p.Metadata[panyl.MetadataTimestamp]; ok {
 		out.WriteString(fmt.Sprintf("%s ", ts.(time.Time).Local().Format("2006-01-02 15:04:05.000")))
 	}
 
 	// application
-	if application := p.Metadata.StringValue(panyl.Metadata_Application); application != "" {
+	if application := p.Metadata.StringValue(panyl.MetadataApplication); application != "" {
 		out.WriteString(fmt.Sprintf("| %s | ", application))
 	}
 
@@ -41,17 +41,17 @@ func (o *Output) OnResult(p *panyl.Process) (cont bool) {
 	}
 
 	// format
-	if format := p.Metadata.StringValue(panyl.Metadata_Format); format != "" {
+	if format := p.Metadata.StringValue(panyl.MetadataFormat); format != "" {
 		out.WriteString(fmt.Sprintf("(%s) ", format))
 	}
 
 	// category
-	if category := p.Metadata.StringValue(panyl.Metadata_Category); category != "" {
+	if category := p.Metadata.StringValue(panyl.MetadataCategory); category != "" {
 		out.WriteString(fmt.Sprintf("{{%s}} ", category))
 	}
 
 	// message
-	if msg := p.Metadata.StringValue(panyl.Metadata_Message); msg != "" {
+	if msg := p.Metadata.StringValue(panyl.MetadataMessage); msg != "" {
 		out.WriteString(msg)
 	} else if p.Line != "" {
 		out.WriteString(p.Line)
