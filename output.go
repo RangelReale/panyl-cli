366b27d40e589c79c1afc93986724bce1287294f
Rangel Reale <rangelspam@gmail.com>
2024-12-15 08:27:34 -0300

    rename Process to Item

--- output.go
+++ output.go
@@ -19,22 +19,22 @@ func NewOutput() *Output {
 	return &Output{}
 }
 
-func (o *Output) OnResult(ctx context.Context, p *panyl.Item) (cont bool) {
+func (o *Output) OnResult(ctx context.Context, item *panyl.Item) (cont bool) {
 	var out bytes.Buffer
 
 	// level
-	level := p.Metadata.StringValue(panyl.MetadataLevel)
+	level := item.Metadata.StringValue(panyl.MetadataLevel)
 	if level == "" {
 		level = "unknown"
 	}
 
 	// timestamp
-	if ts, ok := p.Metadata[panyl.MetadataTimestamp]; ok {
+	if ts, ok := item.Metadata[panyl.MetadataTimestamp]; ok {
 		out.WriteString(fmt.Sprintf("%s ", ts.(time.Time).Local().Format("2006-01-02 15:04:05.000")))
 	}
 
 	// application
-	if application := p.Metadata.StringValue(panyl.MetadataApplication); application != "" {
+	if application := item.Metadata.StringValue(panyl.MetadataApplication); application != "" {
 		out.WriteString(fmt.Sprintf("| %s | ", application))
 	}
 
@@ -44,22 +44,22 @@ func (o *Output) OnResult(ctx context.Context, p *panyl.Item) (cont bool) {
 	}
 
 	// format
-	if format := p.Metadata.StringValue(panyl.MetadataFormat); format != "" {
+	if format := item.Metadata.StringValue(panyl.MetadataFormat); format != "" {
 		out.WriteString(fmt.Sprintf("(%s) ", format))
 	}
 
 	// category
-	if category := p.Metadata.StringValue(panyl.MetadataCategory); category != "" {
+	if category := item.Metadata.StringValue(panyl.MetadataCategory); category != "" {
 		out.WriteString(fmt.Sprintf("{{%s}} ", category))
 	}
 
 	// message
-	if msg := p.Metadata.StringValue(panyl.MetadataMessage); msg != "" {
+	if msg := item.Metadata.StringValue(panyl.MetadataMessage); msg != "" {
 		out.WriteString(msg)
-	} else if p.Line != "" {
-		out.WriteString(p.Line)
-	} else if len(p.Data) > 0 {
-		dt, err := json.Marshal(p.Data)
+	} else if item.Line != "" {
+		out.WriteString(item.Line)
+	} else if len(item.Data) > 0 {
+		dt, err := json.Marshal(item.Data)
 		if err != nil {
 			fmt.Printf("Error marshaling data to json: %s\n", err.Error())
 			return
